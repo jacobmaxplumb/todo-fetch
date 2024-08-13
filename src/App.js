dee5ba59629f45d5d26ab8b7c94da3d00af9faf0
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-08-13 18:35:57 -0400

    made way to add todo item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const addTodo = async () => {
+    const response = await fetch('http://localhost:9000/todos', {
+      method: 'POST',
+      body: JSON.stringify({text: text, completed: false})
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
     </div>
   );
 }
